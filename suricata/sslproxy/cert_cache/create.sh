d83353e4b0b2cdbfef2ab4c8f58496cd143c7966
Damigos Antonios <damigosantonios@daminet.gr>
2026-09-18 08:19:50 +0300

    Update create.sh
    Update creating CA cert to rsa for maxumum combatibity. Keep in comment the EC.

--- suricata/sslproxy/cert_cache/create.sh
+++ suricata/sslproxy/cert_cache/create.sh
@@ -1,9 +1,16 @@
 # 1. Generate the ECDSA P-256 (prime256v1) private key
-openssl ecparam -name prime256v1 -genkey -out utmca.key
+#EC keys. Note that some browser will use EKU flags and may not work. Uncomment if you still want to use it.
+#openssl ecparam -name prime256v1 -genkey -out utmca.key
+
+openssl genrsa -out utmca.key 2048
 
 # 2. Generate the self-signed CA certificate
 openssl req -new -x509 -days 3650 -config ca.cnf -key utmca.key -out utmca.crt
 
 
+
 #Create leaf key
-openssl ecparam -name prime256v1 -genkey -out leaf.key
+#EC leaf.
+#openssl ecparam -name prime256v1 -genkey -out leaf.key
+
+openssl genrsa -out leaf.key 2048
